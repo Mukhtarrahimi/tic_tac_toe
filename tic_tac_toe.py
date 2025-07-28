1674f892e45ceaaafbe14159b12f53203d036f7d
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-07-28 15:32:05 +0430

    is_winner function code.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,6 +2,7 @@ from termcolor import colored
 
 board = [1,2,3,'X',5,6,7,'O',9]
 computer, player = "O", "X"
+winner = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (3,5,8), (0,4,8), (2,4,6))
 
 def print_board():
     j = 1
@@ -23,6 +24,19 @@ def make_move(brd, plyr, mve, Undo = False):
     if can_move(brd, mve):
         brd[mve -1] = plyr
         win = is_winner(brd, plyr)
+        
+def is_winner(brd, plyr):
+    win = True
+    for tup in winner:
+        win = True
+        for j in tup:
+            if brd[j] != plyr:
+                win = False
+                break
+            if win:
+                break
+        return True
+            
 
 def can_move(brd, mve):
     if mve in range(1, 10) and isinstance(brd[mve-1], int):
