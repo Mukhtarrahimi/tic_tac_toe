c4b3d5b726bdb8934ce8fb5e5a24a3409a11f835
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-07-28 20:47:49 +0430

    code update.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,54 +1,45 @@
 from termcolor import colored
+
 board = list(range(1, 10))
-winners = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+winners = ((0, 1, 2), (3, 4, 5), (6, 7, 8),
+           (0, 3, 6), (1, 4, 7), (2, 5, 8),
+           (0, 4, 8), (2, 4, 6))
 moves = ((1, 7, 3, 9), (5,), (2, 4, 6, 8))
 
-def print_board():
-    j = 1
-    for i in board:
-        end = " "
-        if j % 3 == 0:
-            end = "\n\n"
-        if i == "X":
-            print(colored(f"[{i}]", "red"), end=end)
-        elif i == "O":
-            print(colored(f"[{i}]", "blue"), end=end)
-        else:
-            print(f"[{i}]", end=end)
-        j += 1
+player, computer = "X", "O"
 
+def print_board():
+    for i in range(0, 9, 3):
+        for j in range(3):
+            val = board[i + j]
+            if val == "X":
+                print(colored(f"[{val}]", "red"), end=" ")
+            elif val == "O":
+                print(colored(f"[{val}]", "blue"), end=" ")
+            else:
+                print(f"[{val}]", end=" ")
+        print("\n---------")
 
 def make_move(brd, plyr, mve, undo=False):
     if can_move(brd, mve):
-        brd[mve-1] = plyr
+        brd[mve - 1] = plyr
         win = is_winner(brd, plyr)
         if undo:
-            brd[mve-1] = mve
+            brd[mve - 1] = mve
         return True, win
     return False, False
 
-
 def can_move(brd, mve):
-    if mve in range(1, 10) and isinstance(brd[mve-1], int):
-        return True
-    return False
+    return mve in range(1, 10) and isinstance(brd[mve - 1], int)
 
 def is_winner(brd, plyr):
-    win = True
     for tup in winners:
-        win = True
-        for j in tup:
-            if brd[j] != plyr:
-                win = False
-                break
-        if win:
-            break
-    return win
-
+        if all(brd[i] == plyr for i in tup):
+            return True
+    return False
 
 def has_empty_space():
     return board.count("X") + board.count("O") != 9
-
 
 def computer_move():
     mv = -1
@@ -56,13 +47,11 @@ def computer_move():
         if make_move(board, computer, i, True)[1]:
             mv = i
             break
-    # For that the user cannot win
     if mv == -1:
         for j in range(1, 10):
             if make_move(board, player, j, True)[1]:
                 mv = j
                 break
-    # Move
     if mv == -1:
         for tup in moves:
             for m in tup:
@@ -71,25 +60,31 @@ def computer_move():
                     break
     return make_move(board, computer, mv)
 
-
-player, computer = "X", "O"
+# ---- Game Start ----
 print("Player: X\nComputer: O\n")
+
 while has_empty_space():
     print_board()
     try:
         move = int(input("Choose your move (1-9): "))
     except ValueError:
-        print("Invalid input! Please enter a number.")
+        print("Invalid input!\n")
         continue
+
     moved, won = make_move(board, player, move)
     if not moved:
-        print("Invalid number! Try again!")
+        print("Invalid move! Try again.\n")
         continue
     if won:
+        print_board()
         print(colored("You won!", "green"))
         break
-    elif computer_move()[1]:
+    moved, won = computer_move()
+    if won:
+        print_board()
         print(colored("You lose!", "yellow"))
         break
 
-print_board()
+else:
+    print_board()
+    print(colored("It's a draw!", "cyan"))
